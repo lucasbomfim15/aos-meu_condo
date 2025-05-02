648a3818bac82960bfa3272a419a374b7ed3b492
lucasbomfim15 <bmfmlucas@gmail.com>
2025-05-02 20:46:35 -0300

    first commit

---
+++ src/main/java/com/aos/meu_condo/MeuCondoApplication.java
@@ -0,0 +1,13 @@
+package com.aos.meu_condo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuCondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuCondoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aos/meu_condo/MeuCondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aos.meu_condo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuCondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
