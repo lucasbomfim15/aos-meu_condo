e1dc806e5e65781b79dd8ffa90619132adf65e66
Vanessa <vanessalins356@gmail.com>
2025-05-03 21:15:54 -0300

    enviando

---
+++ src/main/java/com/aos/meu_condo/modules/condominium/services/CondominiumService.java
@@ -0,0 +1,41 @@
+package com.aos.meu_condo.modules.condominium.services;
+
+import com.aos.meu_condo.modules.condominium.model.Condominium;
+import com.aos.meu_condo.modules.condominium.repository.CondominiumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CondominiumService {
+    @Autowired
+   private CondominiumRepository condominiumRepository;
+
+    public Condominium create(Condominium condominium){
+        return condominiumRepository.save(condominium);
+    }
+    public Optional<Condominium> findbycnpj(String cnpj){
+        return condominiumRepository.findByCnpj(cnpj);
+    }
+    public Condominium updatecondo(Long id, Condominium updatecondominium){
+        if(!condominiumRepository.existsById(id)){
+            throw new RuntimeException("Condominium not found with id: " + id);
+        }
+        updatecondominium.setId(id);
+        return condominiumRepository.save(updatecondominium);
+    }
+
+    public List<Condominium> findAll(){
+        return condominiumRepository.findAll();
+    }
+
+    public void deleteById(Long id) {
+        if (!condominiumRepository.existsById(id)) {
+            throw new RuntimeException("Condominium not found with id: " + id);
+        }
+        condominiumRepository.deleteById(id);
+    }
+
+}
